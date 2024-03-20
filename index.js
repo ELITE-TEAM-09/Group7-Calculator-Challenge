883e0bd4e57b150cbd861c13b2a475e820b808c2
jeangathoni8 <jeangathoni39@gmail.com>
2024-03-20 09:54:36 +0300

    "calcfriend"

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write JavaScript code below 
+
+//Variable to our current input
+
+//Assume this  is the user's input
+
+//Write  a function that takes in our input
+
+//Append the value to our current input
+
+//Update the input field with our current input
+
+//
